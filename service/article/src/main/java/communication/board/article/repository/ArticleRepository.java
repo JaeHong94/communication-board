c01cbdf2b55298ca0bfb2669c8114a6ab499710d
JaeHong94 <moonshin123@naver.com>
2025-03-11 04:35:22 +0900

    Article CRUD API 구현 및 RestClient를 활용한 Test 코드 작성

---
+++ service/article/src/main/java/communication/board/article/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package communication.board.article.repository;
+
+import communication.board.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
---
+++ service/article/src/main/java/communication/board/article/service/ArticleService.java
@@ -0,0 +1,44 @@
+package communication.board.article.service;
+
+import communication.board.article.entity.Article;
+import communication.board.article.repository.ArticleRepository;
+import communication.board.article.service.request.ArticleCreateRequest;
+import communication.board.article.service.request.ArticleUpdateRequest;
+import communication.board.article.service.response.ArticleResponse;
+import communication.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ArticleResponse create(ArticleCreateRequest request) {
+        // snowflake를 활용해서 게시판 생성
+        Article article = articleRepository.save(
+                Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
+        );
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest request) {
+        Article article = articleRepository.findById(articleId).orElseThrow(); // 없는 게시판일 경우 예외 던지기
+        article.update(request.getTitle(), request.getContent());
+        return ArticleResponse.from(article);
+    }
+
+    public ArticleResponse read(Long articleId) {
+        return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long articleId) {
+        articleRepository.deleteById(articleId);
+    }
+
+}
