04ecd339248569eeccb0e7fee807b0de08c9c8e5
JaeHong94 <moonshin123@naver.com>
2025-03-22 03:04:10 +0900

    feat(like): Implement like and unlike functionality

---
+++ service/like/src/main/java/communication/board/like/service/ArticleLikeService.java
@@ -0,0 +1,39 @@
+package communication.board.like.service;
+
+import communication.board.common.snowflake.Snowflake;
+import communication.board.like.entity.ArticleLike;
+import communication.board.like.repository.ArticleLikeRepository;
+import communication.board.like.service.response.ArticleLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+
+    public ArticleLikeResponse read(Long articleId, Long userId) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public void like(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+    }
+
+    @Transactional
+    public void unlike(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLikeRepository::delete);
+    }
+}
